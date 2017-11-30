ffb4819be47d2115baf03ca5184e7882f5746ce3
Joe Turner <joe@oampo.co.uk>
2017-11-30 12:07:51 +0000

    Add solution

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,3 +1,16 @@
+import reducer from './reducer';
+
 describe('reducer', () => {
-    it('should handle unknown actions correctly');
+    it('should handle unknown actions correctly', () => {
+        const state = {
+            comments: [
+                {
+                    author: 'Foo bar',
+                    comment: 'Hello world'
+                }
+            ]
+        };
+        const newState = reducer(state, {type: '__UNKNOWN'});
+        expect(newState).toBe(state);
+    });
 });
